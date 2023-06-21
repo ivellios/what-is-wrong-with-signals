f0f2559426a587f56178c3b82400a67ed26b9b3f
Janusz Kamienski <januszk@wix.com>
2023-06-21 16:20:06 +0200

    remove non-relevant test

--- project/notifications/tests.py
+++ project/notifications/tests.py
@@ -1,19 +1,1 @@
-from unittest import mock
-
-import pytest
-
-from todo.models import ToDo
-
-
 # Create your tests here.
-
-
-@pytest.mark.django_db
-@mock.patch("logging.Logger.info")
-def test_archiving_todo_calls_info(logging_mock: mock.Mock):
-    todo = ToDo.objects.create(title="Some todo", status="active")
-    logging_mock.reset_mock()
-
-    todo.archive()
-
-    logging_mock.assert_called_with(f"TODO has been archived title=%s", todo.title)
